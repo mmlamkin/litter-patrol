8a40b94b782a7fdef3d85dbdb41f1104d15edc24
Mary Lamkin <mmlamkin@gmail.com>
2018-05-30 15:59:27 -0700

    litter game

--- src/App.js
+++ src/App.js
@@ -21,37 +21,45 @@ class App extends Component {
       });
     }, 1500);
   }
+  // sets up 9 bins and calls them to be changed every 1.5s
 
   getBinsState() {
     let bins = [];
     for (let i = 0; i < 9; i++){
       bins.push({ isTrashVisible: (Math.round(Math.random()) ? true : false )});
     }
-
     return bins;
   }
+  // returns the state of each bin in the array t if theres trash and f if not
 
-  onTrashClicked = () => {
-    // Fill this in!
+  onTrashClicked = (index) => {
+    let updatedBins = this.state.bins;
+
+    updatedBins[index].present = true
+    let points = this.state.points
+    this.setState( {
+    points: points+=1
+  });
   }
 
   render() {
-    const bins = this.state.bins.map((bin, index) => {
+
+    let bins = this.state.bins.map((bin, index) => {
       return (
-        <Trash key={`trash-${index}`} />
+        <Trash key={`trash-${index}`} trashPresent={bin.isTrashVisible} trashClicked={this.onTrashClicked} index={index}/>
       );
     });
 
     return (
       <div className="App">
-        <section className="overall-data">
-          <h1>Litter Patrol</h1>
-          <h2>Points: { this.state.points }</h2>
-        </section>
+      <section className="overall-data">
+      <h1>Litter Patrol</h1>
+      <h2>Points: { this.state.points }</h2>
+      </section>
 
-        <section className="bins">
-          { bins }
-        </section>
+      <section className="bins">
+      { bins }
+      </section>
       </div>
     );
   }
--- src/components/Trash.js
+++ src/components/Trash.js
@@ -3,10 +3,22 @@ import '../App.css';
 import TrashIcon from '../trash.svg';
 
 class Trash extends Component {
+
+  onHasTrash = () => {
+    this.props.trashClicked(this.props.index);
+  }
+
+  isVisible = () => {
+    if (this.props.trashPresent) {
+      return <img src={ TrashIcon } alt="Trash" className="trash" onClick={this.onHasTrash}></img>
+    }
+  }
+
   render() {
+
     return (
       <div className="bin">
-        <img src={ TrashIcon } alt="Trash" className="trash"></img>
+        {this.isVisible()}
       </div>
     );
   }
